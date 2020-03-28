e3f005bae471324dc5d54fda372831531498cd0d
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-03-28 11:25:19 +0900

    Create module_test.py

---
+++ DayStudy/ModuleApplicationsPackage/module_test.py
@@ -0,0 +1,11 @@
+#File : module_test.py
+
+def add(a,b):
+  return a+b
+
+def f():
+  print("Python is becoming popular.")
+
+if __name__=="__main__":
+  print(add(1,10))    #11
+  f()    #Python is becoming popular.
