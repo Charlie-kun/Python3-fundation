05ba0087027b60d0e4d888e9c121d1d735e36aaf
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-10-19 21:06:23 +0900

    Update class4.py

--- DayStudy/AI_Study/class4.py
+++ DayStudy/AI_Study/class4.py
@@ -1,7 +1,55 @@
+#AI Class4 study
 def get_sum(start, end):
-  sum=0
-  for i in range(start, end+1):
-    sum+=i
-  return sum
-  
-  
+    sum=0
+    for i in range(start, end+1):
+        sum+=i
+    return sum
+
+value=get_sum(1,10)
+print(value)
+
+def square(n):
+    return(n*n)
+
+print(square(10))
+
+def get_min(x,y):
+    if(x<y):
+        return x
+    else:
+        return y
+
+print(get_min(10,20))
+
+def happyBirthday():
+    print("HB")
+    print("HB")
+    print("dear my friend")
+    return
+
+happyBirthday()
+
+def FtoC():
+    x=float(input("input Fahrenheit : "))
+    f=5/9*(x-32)
+    return f
+
+print(FtoC())
+
+def is_prime():
+    x=int(input("input number : "))
+    for i in range(2,x):
+        if(x%i==0):
+            return False
+    return True
+
+print(is_prime())
+
+import math
+
+def sphereVolume():
+    r=int(input("imput radius : "))
+    v=(4/3)*math.pi*r**3
+    return v
+
+print(sphereVolume())
