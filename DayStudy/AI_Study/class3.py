75255fb33b4b7867f5dda5bc1ab555f9c0857106
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-10-25 20:34:33 +0900

    Update class3.py

--- DayStudy/AI_Study/class3.py
+++ DayStudy/AI_Study/class3.py
@@ -31,7 +31,7 @@ for i in range(1,101):   #Important is last 101.
     sum+=i
 print("1+2+...+100 = ", sum)
 print("1+2+...+100 =" +str(sum))
-
+'''
 import turtle
 t=turtle.Pen()
 
@@ -43,3 +43,29 @@ while True:
     if direction=="right":
         t.right(60)
         t.forward(50)
+'''
+x=int(input("First integer : "))
+y=int(input("Second integer : "))
+
+if (x>y):
+    max=x;
+else :
+    max=y;
+print("Big integer is ",max);
+
+str=input("Input string : ")
+length=len(str)
+if length%2==1:
+    ch1=str[length//2]
+    print("Center char is ", ch1)
+else:
+    ch1=str[length//2-1]
+    ch2=str[length//2]
+    print("Center char is", ch1, ch2)
+
+credits=float(input('input your score : '))
+gpa=float(input("Input your grade point average : "))
+if credits>=140 and gpa>=2.0:
+    print("Possible to graduate!")
+else :
+    print("Impossible to graduate!")
