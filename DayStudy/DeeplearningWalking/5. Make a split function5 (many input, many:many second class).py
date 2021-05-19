1dbe579f06c0b680ffe17800279d4b1238ecc13a
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2021-05-19 21:20:05 +0900

    Update 5. Make a split function5 (many input, many:many second class).py

--- DayStudy/DeeplearningWalking/5. Make a split function5 (many input, many:many second class).py
+++ DayStudy/DeeplearningWalking/5. Make a split function5 (many input, many:many second class).py
@@ -1,9 +1,9 @@
 import numpy as np
 dataset=np.array([[1,2,3,4,5,6,7,8,9,10], [11,12,13,14,15,16,17,18,19,20], [21,22,23,24,25,26,27,28,29,30]])
-print("dataset.shape : ", dataset.shape)
-dataset=np.transpose(dataset)
+print("dataset.shape : ", dataset.shape)    #3,10
+dataset=np.transpose(dataset)       #Change array position
 print(dataset)
-print("dataset.shape : ", dataset.shape)
+print("dataset.shape : ", dataset.shape)   #10,3
 #print(len(dataset))  #3
 
 def split_xy5(datset, time_steps, y_column):
